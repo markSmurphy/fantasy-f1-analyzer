1e57118e63d1472c7d62800603d9d132c6f40167
markSmurphy <mark.murphy@techemail.com>
2022-05-07 18:27:21 +0100

    Improved code annotation

--- analysis.js
+++ analysis.js
@@ -1,10 +1,15 @@
 const debug = require('debug')('fantasy-f1-analyzer-analysis');
 debug('Entry: [%s]', __filename);
 
+// Extract all Constructors from the raw data
 function getConstructors(data) {
    debug('getConstructors::Entry');
+   // Initialise constructors array
    let constructors = [];
+
+   // Iterate through *all* data elements
    data.players.forEach(element => {
+      // If it's a constructor then add it to the array
       if (element.is_constructor === true) {
          constructors.push(element);
       }
@@ -13,10 +18,15 @@ function getConstructors(data) {
    return(constructors);
 }
 
+// Extract all Drivers from the raw data
 function getDrivers(data) {
    debug('getDrivers::Entry');
+   // Initialise drivers array
    let drivers = [];
+
+   // Iterate through *all* data elements
    data.players.forEach(element => {
+      // If it's *not* a constructor then it's a drivers, so  add it to the array
       if (element.is_constructor === false) {
          drivers.push(element);
       }
