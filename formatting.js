641c32fc45e2ccf5b0934e34bc5a54eb7303f9d5
markSmurphy <mark.murphy@techemail.com>
2022-05-30 10:10:51 +0100

    Sorted Summary output lists

--- formatting.js
+++ formatting.js
@@ -7,7 +7,7 @@ const chalk = require('chalk');
 function applyTeamColours(text, team_abbreviation) {
    // Handle missing team_abbreviation
    if (team_abbreviation == undefined) {
-      return(text);
+      return (text);
    } else {
       let response = '';
 
@@ -61,4 +61,12 @@ function applyTeamColours(text, team_abbreviation) {
    }
 }
 
-module.exports = { applyTeamColours };
+function sortByPoints(list) {
+   list.sort((a, b) => {
+      return (b.season_score - a.season_score);
+   });
+
+   return (list);
+}
+
+module.exports = { applyTeamColours, sortByPoints };
--- summary.js
+++ summary.js
@@ -7,18 +7,19 @@ const formatting = require('./formatting');
 // Console output formatting for columns and colours
 const columnify = require('columnify');
 
-// Initialise console colours
-const chalk = require('chalk');
-
-function displayCurrentStandings(data) {
+function displayCurrentStandings(f1DataSet) {
     debug('displayCurrentStandings() :: Entry');
+
+    let drivers = formatting.sortByPoints(f1DataSet.drivers);
+    let constructors = formatting.sortByPoints(f1DataSet.constructors);
+
     let currentStandings = {
         drivers: [],
         constructors: []
     };
 
     // Iterate through Drivers
-    data.drivers.forEach(element => {
+    drivers.forEach(element => {
         let details = {};
         details.driver = formatting.applyTeamColours(element.display_name, element.team_abbreviation);
         details.points = formatting.applyTeamColours(element.season_score, element.team_abbreviation);
@@ -29,7 +30,7 @@ function displayCurrentStandings(data) {
     debug('Got %i drivers', currentStandings.drivers.length);
 
     // Iterate through Constructors
-    data.constructors.forEach(element => {
+    constructors.forEach(element => {
         let details = {};
         details.constructor = formatting.applyTeamColours(element.display_name, element.team_abbreviation);
         details.points = formatting.applyTeamColours(element.season_score, element.team_abbreviation);
@@ -40,13 +41,11 @@ function displayCurrentStandings(data) {
     debug('Got %i constructors', currentStandings.constructors.length);
 
     // Display summary of current standing
-    console.log(chalk.whiteBright('---- Drivers ----\r\n'))
     let driversColumns = columnify(currentStandings.drivers, {
         columnSplitter: ' | ',
     });
     console.log(driversColumns + '\r\n\r\n');
 
-    console.log(chalk.whiteBright('---- Constructors ----\r\n'))
     let constructorsColumns = columnify(currentStandings.constructors, {
         columnSplitter: ' | ',
     });
