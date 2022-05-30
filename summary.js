b8d325d51c3fa9ab8301b4b1240e9c69bd85da9e
markSmurphy <mark.murphy@techemail.com>
2022-05-30 11:09:50 +0100

    Summary now formatted into columns

--- summary.js
+++ summary.js
@@ -7,49 +7,44 @@ const formatting = require('./formatting');
 // Console output formatting for columns and colours
 const columnify = require('columnify');
 
+// Platform independent end-of-line character
+const EOL = require('os').EOL;
+
 function displayCurrentStandings(f1DataSet) {
     debug('displayCurrentStandings() :: Entry');
 
+    // Extract both Drivers & Constructors lists and sort them
     let drivers = formatting.sortByPoints(f1DataSet.drivers);
     let constructors = formatting.sortByPoints(f1DataSet.constructors);
 
-    let currentStandings = {
-        drivers: [],
-        constructors: []
-    };
+    // Initialise the object we're going to store the Summary in
+    let summary = [];
 
-    // Iterate through Drivers
-    drivers.forEach(element => {
-        let details = {};
-        details.driver = formatting.applyTeamColours(element.display_name, element.team_abbreviation);
-        details.points = formatting.applyTeamColours(element.season_score, element.team_abbreviation);
+    // Loop through the Drivers & Constructors creating two columns (for the first 10 rows, then we run out of Constructors)
+    for (let i = 0; i < drivers.length - 1; i++) {
+        let row = {
+            constructor: formatting.applyTeamColours(constructors[i]?.display_name, constructors[i]?.team_abbreviation),
+            constructorPoints: formatting.applyTeamColours(constructors[i]?.season_score, constructors[i]?.team_abbreviation),
+            driver: formatting.applyTeamColours(drivers[i].display_name, drivers[i].team_abbreviation),
+            driverPoints: formatting.applyTeamColours(drivers[i].season_score, drivers[i].team_abbreviation)
+        }
 
-        // Save details into Current Standing object
-        currentStandings.drivers.push(details);
-    });
-    debug('Got %i drivers', currentStandings.drivers.length);
-
-    // Iterate through Constructors
-    constructors.forEach(element => {
-        let details = {};
-        details.constructor = formatting.applyTeamColours(element.display_name, element.team_abbreviation);
-        details.points = formatting.applyTeamColours(element.season_score, element.team_abbreviation);
-
-        // Save details into Current Standing object
-        currentStandings.constructors.push(details);
-    });
-    debug('Got %i constructors', currentStandings.constructors.length);
+        summary.push(row);
+    }
 
     // Display summary of current standing
-    let driversColumns = columnify(currentStandings.drivers, {
-        columnSplitter: ' | ',
+    let columns = columnify(summary, {
+        config: {
+            constructorPoints: {
+                headingTransform: () => {return('POINTS')},
+                columnSplitter: ' | '
+            },
+            driverPoints: {
+                headingTransform: () => {return('POINTS')}
+            }
+        }
     });
-    console.log(driversColumns + '\r\n\r\n');
-
-    let constructorsColumns = columnify(currentStandings.constructors, {
-        columnSplitter: ' | ',
-    });
-    console.log(constructorsColumns);
+    console.log(columns + EOL);
 }
 
 module.exports = { displayCurrentStandings };
